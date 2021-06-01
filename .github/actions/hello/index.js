b7826fe5533de7d75de5e20395eba2101347043b
rahulbist-devops <rahul.bist@bain.com>
2021-06-01 19:19:16 +0530

    react app

--- .github/actions/hello/index.js
+++ .github/actions/hello/index.js
@@ -3,11 +3,11 @@ const github = require("@actions/github");
 
 try {
 //  throw( new Error("some error message"));
-  const name = core.getInput('who-to-greet');
+  const name = core.getInput("who-to-greet");
   console.log('Hello ${name}');  
   const time = new Date();
   core.setOutput("time", time.toTimeString());  
-  console.log(JSON.stringify(github, null, '\t'));
+  console.log(JSON.stringify(github, null, "\t"));
 } catch(error) {
     core.setFailed(error.message);
 }
